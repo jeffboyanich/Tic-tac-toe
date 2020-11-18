a7b200d836271cb5ea3aead45a2296224f7cc181
jeffboyanich <jsboyanich123@gmail.com>
2020-11-18 01:44:57 -0800

    Updated gameplay functions and some styling

--- index.js
+++ index.js
@@ -1,24 +1,58 @@
-const gameBoard = document.getElementById('gameboard');
-
-
 const displayBoard = (() => {
+    const gameBoard = document.getElementById('gameboard');
     for (let i = 0; i < 9; i++) {
         index = i;
-        console.log(index);
         const boardSpace = document.createElement('div');
         boardSpace.classList.add('board-space');
         boardSpace.dataset.index = index;
         gameBoard.appendChild(boardSpace);
+        if (index == 0 || index == 3) {
+            boardSpace.classList.add('bottom-right');
+        }else if (index == 1 || index == 4) {
+            boardSpace.classList.add('bottom');
+        }else if (index == 2 || index == 5) {
+            boardSpace.classList.add('bottom-left');
+        }else if (index == 6) {
+            boardSpace.classList.add('right');
+        }else if (index == 8) {
+            boardSpace.classList.add('left');
         }
+    };
+    const boardSpaces = document.querySelectorAll('.board-space');
     const boardArr = [];
     for(i = 0; i < 9; i++) {
         boardArr.push('');
     };
-    console.log(boardArr);
-    return {boardArr};
+    function resetDisplay() {
+        boardSpaces.forEach(function (item) {
+            item.textContent = '';
+        });
+        gameOverPopup.style.display = 'none';
+    }
+    const resetBoardArr = () => {
+        for (i = 0; i < 9; i++) {
+            boardArr[i] = '';
+        }
+    }
+    const gameOverPopup = document.getElementById('overlay');
+    function gameOverDisplay(winner) {
+        let popupContent = document.createElement('div');
+        popupContent.classList.add('popup-content');
+        popupContent.textContent = winner + ' won this time!';
+        gameOverPopup.appendChild(popupContent);
+        let continueBtn = document.createElement('button');
+        continueBtn.textContent = 'Yes';
+        continueBtn.addEventListener('click', function() {
+            resetDisplay();
+            resetBoardArr();
+            playGame.reset();
+            popupContent.remove();
+        });
+        popupContent.append(continueBtn);
+        gameOverPopup.style.display = 'block';
+    }
+    return {boardArr, resetDisplay, gameOverDisplay};
 })();
-
-
 
 const createPlayers = (name, marker, turn, value) => {
     const changeTurn = function() {
@@ -38,8 +72,8 @@ const createPlayers = (name, marker, turn, value) => {
 };
 
 const playGame = (() => {
-    const player1 = createPlayers('playerOne', 'X', true, 1);
-    const player2 = createPlayers('playerTwo', 'O', false, 2);
+    const player1 = createPlayers('Player 1', 'X', true, 1);
+    const player2 = createPlayers('Player 2', 'O', false, 2);
     const boardSpaces = document.querySelectorAll('.board-space');
         boardSpaces.forEach(function(item) {
             item.addEventListener('click', function(e) {
@@ -48,17 +82,22 @@ const playGame = (() => {
                     player2.mark(e);
                     player1.changeTurn();
                     player2.changeTurn();
-                    console.log(displayBoard.boardArr);
                     checkWin();
+                    console.log(displayBoard.boardArr);
                 }    
             });
         });
     let winCount = 0;
+    let winner = '';
     function declareWin() {
         if (winCount == 3) {
-            console.log('player1 wins!')
+            winner = `${player1.name}`;
+            displayBoard.gameOverDisplay(winner);
+            console.log(winCount);
         }else if (winCount == 15) {
-            console.log('player2 wins!')
+            winner = `${player2.name}`;
+            displayBoard.gameOverDisplay(winner);
+            console.log(winCount);
         }
         winCount = 0;
         };
@@ -66,7 +105,6 @@ const playGame = (() => {
         for (i = 0; i < 3; i++) {
             if (displayBoard.boardArr[i] == 1) {
                 winCount++;
-                console.log(winCount);
             }else if (displayBoard.boardArr[i] == 2) {
                 winCount += 5;
             }
@@ -75,7 +113,6 @@ const playGame = (() => {
         for (i = 3; i < 6; i++) {
             if (displayBoard.boardArr[i] == 1) {
                 winCount++;
-                console.log(winCount);
             }else if (displayBoard.boardArr[i] == 2) {
                 winCount += 5;
             }
@@ -85,7 +122,6 @@ const playGame = (() => {
         for (i = 6; i < 9; i++) {
             if (displayBoard.boardArr[i] == 1) {
                 winCount++;
-                console.log(winCount);
             }else if (displayBoard.boardArr[i] == 2) {
                 winCount += 5;
             }
@@ -93,22 +129,57 @@ const playGame = (() => {
         declareWin();
     };
     const columnWin = () => {
-        for (i = 0; i < 7; i + 3) {
+        for (i = 0; i < 7; i += 3) {
             if (displayBoard.boardArr[i] == 1) {
                 winCount++;
-                console.log(winCount);
+            }else if (displayBoard.boardArr[i] == 2) {
+                winCount += 5;
+            }
+        }
+        declareWin();
+        for (i = 1; i < 8; i += 3) {
+            if (displayBoard.boardArr[i] == 1) {
+                winCount++;
+            }else if (displayBoard.boardArr[i] == 2) {
+                winCount += 5;
+            }
+        }
+        declareWin();
+        for (i = 2; i < 9; i += 3) {
+            if (displayBoard.boardArr[i] == 1) {
+                winCount++;
             }else if (displayBoard.boardArr[i] == 2) {
                 winCount += 5;
             }
         }
         declareWin();
     }
-
-    
+    const diagonalWin = () => {
+        for (i = 0; i < 9; i += 4) {
+            if (displayBoard.boardArr[i] == 1) {
+                winCount++;
+            }else if (displayBoard.boardArr[i] == 2) {
+                winCount += 5;
+            }
+        }
+        declareWin();
+        for (i = 2; i < 7; i += 2) {
+            if (displayBoard.boardArr[i] == 1) {
+                winCount++;
+            }else if (displayBoard.boardArr[i] == 2) {
+                winCount += 5;
+            }
+        }
+        declareWin();
+    }
     function checkWin() {
       rowWin();
       columnWin();
+      diagonalWin();
     }
-
+    const reset = () => {
+        winner = '';
+    }
+    return {reset}
 })();
 
